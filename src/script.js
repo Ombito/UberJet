d56e22411321d6a1930128b575651e74fe10b2f3
ombito <alvinombito09@gmail.com>
2023-08-02 23:51:27 +0300

    Style checkout page

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+const form = document.getElementById('form')[0];
 const mycards = document.getElementsByClassName("booking-cards");
 
 //add event listener to form
