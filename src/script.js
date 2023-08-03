dd043c44eac451cc97a171ab8ce1a8a83a28c165
ombito <alvinombito09@gmail.com>
2023-08-03 10:53:35 +0300

    Fetch items from db.json server

--- src/script.js
+++ src/script.js
@@ -1,49 +1,49 @@
-const form = document.getElementById('form')[0];
-const mycards = document.getElementsByClassName("booking-cards");
+// const form = document.getElementById('form')[0];
+const mycards = document.getElementsByClassName("booking-cards")[0];
 
-//add event listener to form
-form.addEventListener('submit', submitForm);
+// //add event listener to form
+// form.addEventListener('submit', submitForm);
 
-function submitForm(event) {
-    event.preventDefault(); 
+// function submitForm(event) {
+//     event.preventDefault(); 
 
-    let name = document.getElementById('name').value;
-    let mobile = document.getElementById('mobile').value;
-    let nationalid = document.getElementById('national-id').value;
-    let date = document.getElementById('date').value;
-    let time = document.getElementById('time').value;
-    let departure = document.getElementById('scripts').value;
-    let destination = document.getElementById('drop').value;
+    // let name = document.getElementById('name').value;
+    // let mobile = document.getElementById('mobile').value;
+    // let nationalid = document.getElementById('national-id').value;
+    // let date = document.getElementById('date').value;
+    // let time = document.getElementById('time').value;
+    // let departure = document.getElementById('scripts').value;
+    // let destination = document.getElementById('drop').value;
 
-    let formObj = {
-        name: name,
-        mobile: mobile,
-        nationalid: nationalid,
-        date: date,
-        time: time,
-        departure: departure,
-        destination: destination,
-    };
+    // let formObj = {
+    //     name: name,
+    //     mobile: mobile,
+    //     nationalid: nationalid,
+    //     date: date,
+    //     time: time,
+    //     departure: departure,
+    //     destination: destination,
+    // };
 
-    // Post form data to the JSON server
-    fetch("http://localhost:3000/tickets", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formObj),
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        alert('Flight was booked successfully. Please check your email for details.');
-    })
-    .catch((error) => {
-        console.error("Error posting form data:", error);
-        alert("An error occurred while booking the flight. Please try again later.");
-    });
+    // // Post form data to the JSON server
+    // fetch("http://localhost:3000/tickets", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(formObj),
+    // })
+    // .then((res) => res.json())
+    // .then((data) => {
+    //     console.log(data);
+    //     alert('Flight was booked successfully. Please check your email for details.');
+    // })
+    // .catch((error) => {
+    //     console.error("Error posting form data:", error);
+    //     alert("An error occurred while booking the flight. Please try again later.");
+    // });
 
-}
+// }
 
 //fetch items from json server
 fetch("http://localhost:3000/helicopter")
@@ -61,6 +61,7 @@ function cardsLoad (data) {
             <img src="${i.image}"/> 
             <p>Capacity: ${i.type}</p> 
             <p>Cost: ${i.cost}</p>
+            <a target="_blank" href="./checkout.html">Book</a>
         </div>
         `;
 
