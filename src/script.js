4acf87d7984f8002917e1e68a510e0c75a50612d
ombito <alvinombito09@gmail.com>
2023-08-04 12:10:40 +0300

    Update json API

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const mycards = document.getElementsByClassName("booking-cards")[0];
 // const form = document.getElementById('form')[0];
 
 //fetch items from json server
-fetch("http://localhost:3000/helicopter")
+fetch("https://json-server-ombito.vercel.app/helicopter")
   .then((res) => res.json())
   .then(data => cardsLoad(data))
 
@@ -29,47 +29,47 @@ function cardsLoad (data) {
 }
 
 
-// //add event listener to form
-// form.addEventListener('submit', submitForm);
+//add event listener to form
+form.addEventListener('submit', submitForm);
 
-// function submitForm(event) {
-//     event.preventDefault(); 
+function submitForm(event) {
+    event.preventDefault(); 
 
-    // let name = document.getElementById('name').value;
-    // let mobile = document.getElementById('mobile').value;
-    // let nationalid = document.getElementById('national-id').value;
-    // let date = document.getElementById('date').value;
-    // let time = document.getElementById('time').value;
-    // let departure = document.getElementById('scripts').value;
-    // let destination = document.getElementById('drop').value;
+    let name = document.getElementById('name').value;
+    let mobile = document.getElementById('mobile').value;
+    let nationalid = document.getElementById('national-id').value;
+    let date = document.getElementById('date').value;
+    let time = document.getElementById('time').value;
+    let departure = document.getElementById('scripts').value;
+    let destination = document.getElementById('drop').value;
 
-    // let formObj = {
-    //     name: name,
-    //     mobile: mobile,
-    //     nationalid: nationalid,
-    //     date: date,
-    //     time: time,
-    //     departure: departure,
-    //     destination: destination,
-    // };
+    let formObj = {
+        name: name,
+        mobile: mobile,
+        nationalid: nationalid,
+        date: date,
+        time: time,
+        departure: departure,
+        destination: destination,
+    };
 
-    // // Post form data to the JSON server
-    // fetch("http://localhost:3000/tickets", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(formObj),
-    // })
-    // .then((res) => res.json())
-    // .then((data) => {
-    //     console.log(data);
-    //     alert('Flight was booked successfully. Please check your email for details.');
-    // })
-    // .catch((error) => {
-    //     console.error("Error posting form data:", error);
-    //     alert("An error occurred while booking the flight. Please try again later.");
-    // });
+    // Post form data to the JSON server
+    fetch("https://json-server-ombito.vercel.app/tickets", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formObj),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        alert('Flight was booked successfully. Please check your email for details.');
+    })
+    .catch((error) => {
+        console.error("Error posting form data:", error);
+        alert("An error occurred while booking the flight. Please try again later.");
+    });
 
-// }
+}
 
